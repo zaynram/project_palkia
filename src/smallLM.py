e302d9371010f14391da0735b90b4d805d33e3b5
zaynram <ramdasszayn@gmail.com>
2024-11-24 00:35:11 +0000

    asd

--- src/smallLM.py
+++ src/smallLM.py
@@ -1,1 +1,26 @@
 #this is just the beginning
+
+from collections import Counter
+
+filename = ""
+#text = open(filename)
+text = "the quick brown fox jumped over the lazy dog"
+
+specialChars = ".!?-][}{\"\'\\<>@#$%^&*()+=|,/ "
+
+n = 2
+counts = Counter()
+window = []
+
+exerpt = text.split()
+
+for word in exerpt:
+    clean_word = word.lower().strip(specialChars)
+    if len(window) >= n:
+        counts[tuple(window)] += 1
+        window.pop(0)
+    
+    window.append(clean_word)
+        
+        
+print(counts.most_common(5))
